fe83924d803a7737de9fa688fedda493c0c9bc67
hellmir <128391669+hellmir@users.noreply.github.com>
2023-04-19 23:54:20 +0900

    feat(Class): create Menu domain

---
+++ src/main/java/personal/delivery/domain/Menu.java
@@ -0,0 +1,36 @@
+package personal.delivery.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "menu")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer salesRate;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private String flavor;
+    private Integer portions;
+    private Integer cookingTime;
+    private String menuType;
+    private String foodType;
+    private Boolean popularMenu;
+}
