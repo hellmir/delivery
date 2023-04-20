17de952540ae7b53e99708b8e06b6912b8dc7c27
Sung Hyobin <hellmir610@gmail.com>
2023-04-21 01:06:30 +0900

    delete method

--- src/main/java/personal/delivery/repository/MenuRepository.java
+++ src/main/java/personal/delivery/repository/MenuRepository.java
@@ -1,10 +1,7 @@
 package personal.delivery.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import personal.delivery.domain.Menu;
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    Optional<Menu> findById(Long id);
 }
