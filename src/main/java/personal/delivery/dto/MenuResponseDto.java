f05b899eb9f55d6a26522ccc20e52a1ecefe24a3
Sung Hyobin <hellmir610@gmail.com>
2023-04-21 00:21:32 +0900

    feat(Classes): create Dto and ResponseDto to just transmit data

---
+++ src/main/java/personal/delivery/dto/MenuResponseDto.java
@@ -0,0 +1,23 @@
+package personal.delivery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MenuResponseDto {
+
+    private Long id;
+    private String name;
+    private int price;
+    private int salesRate;
+    private int stock;
+    private String flavor;
+    private int portions;
+    private int cookingTime;
+    private String menuType;
+    private String foodType;
+    private boolean popularMenu;
+}
