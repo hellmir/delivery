c5ec416150d6e29700a1c4c9b4a2ed9e213038ee
Sung Hyobin <hellmir610@gmail.com>
2023-04-23 10:33:46 +0900

    feat(Methods): add getAllMenu, changeMenu and deleteMenu

--- src/main/java/personal/delivery/service/MenuService.java
+++ src/main/java/personal/delivery/service/MenuService.java
@@ -1,12 +1,21 @@
 package personal.delivery.service;
 
+import personal.delivery.dto.MenuChangeDto;
 import personal.delivery.dto.MenuDto;
 import personal.delivery.dto.MenuResponseDto;
 
+import java.util.List;
+
 public interface MenuService {
+
+    MenuResponseDto saveMenu(MenuDto menuDto);
 
     MenuResponseDto getMenu(Long id);
 
-    MenuResponseDto saveMenu(MenuDto menuDto);
+    List<MenuResponseDto> getAllMenu() throws Exception;
+
+    MenuResponseDto changeMenu(MenuChangeDto menuChangeDto) throws Exception;
+
+    String deleteMenu(Long id) throws Exception;
 
 }
