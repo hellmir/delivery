9c1b76b96af353a765740eccce02fffdde17ce41
Sung Hyobin <hellmir610@gmail.com>
2023-04-28 02:24:07 +0900

    Change Menu
    Removed Setters and now the Menu class uses Builder annotation to initialize values and methods to change values.

--- src/main/java/personal/delivery/menu/Menu.java
+++ src/main/java/personal/delivery/menu/Menu.java
@@ -1,15 +1,16 @@
 package personal.delivery.menu;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "menu")
 public class Menu {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,4 +33,61 @@ public class Menu {
     private String menuType;
     private String foodType;
     private Boolean popularMenu;
+
+
+    @Builder
+    public Menu(Long id, String name, Integer price, Integer salesRate, Integer stock, String flavor, Integer portions,
+                Integer cookingTime, String menuType, String foodType, Boolean popularMenu) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.salesRate = salesRate;
+        this.stock = stock;
+        this.flavor = flavor;
+        this.portions = portions;
+        this.cookingTime = cookingTime;
+        this.menuType = menuType;
+        this.foodType = foodType;
+        this.popularMenu = popularMenu;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void changePrice(Integer price) {
+        this.price = price;
+    }
+
+    public void addSalesRate(Integer salesRate) {
+        this.salesRate = salesRate;
+    }
+
+    public void addStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public void changeFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public void changePortions(Integer portions) {
+        this.portions = portions;
+    }
+
+    public void changeCookingTime(Integer cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public void changeMenuType(String menuType) {
+        this.menuType = menuType;
+    }
+
+    public void changeFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public void changePopularMenu(Boolean popularMenu) {
+        this.popularMenu = popularMenu;
+    }
 }
