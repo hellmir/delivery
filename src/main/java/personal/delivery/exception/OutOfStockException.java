bcd693612829c070d94701d02e9dc6271dc457df
Sung Hyobin <hellmir610@gmail.com>
2023-04-29 22:33:50 +0900

    feat(Class): create OutOfStockException class to throw custom exception when the stock is insufficient

---
+++ src/main/java/personal/delivery/exception/OutOfStockException.java
@@ -0,0 +1,8 @@
+package personal.delivery.exception;
+
+public class OutOfStockException extends RuntimeException {
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
