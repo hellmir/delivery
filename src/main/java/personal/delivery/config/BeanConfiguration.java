7488a6d55145e1f86c7ea0c1e97a67e4835bdddc
Sung Hyobin <hellmir610@gmail.com>
2023-04-30 11:56:13 +0900

    fix(Method): change the getAllMenu method to respond to the result of the selectAllMenu method

--- src/main/java/personal/delivery/config/BeanConfiguration.java
+++ src/main/java/personal/delivery/config/BeanConfiguration.java
@@ -2,11 +2,13 @@ package personal.delivery.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfiguration {
 
     @Bean
-    public static ModelMapper modelMapper() {
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
