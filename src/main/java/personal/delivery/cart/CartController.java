b46faf9df68340671bb405aec4adaa440199de35
Sung Hyobin <hellmir610@gmail.com>
2023-05-24 23:31:55 +0900

    feat(Method): create a getCartMenuList method in the CartController

--- src/main/java/personal/delivery/cart/CartController.java
+++ src/main/java/personal/delivery/cart/CartController.java
@@ -3,13 +3,12 @@ package personal.delivery.cart;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import personal.delivery.cart.dto.CartMenuDto;
 import personal.delivery.cart.dto.CartMenuResponseDto;
 import personal.delivery.cart.service.CartService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("carts")
@@ -27,5 +26,13 @@ public class CartController {
 
     }
 
+    @GetMapping()
+    public ResponseEntity<List<CartMenuResponseDto>> getCartMenuList() {
+
+        List<CartMenuResponseDto> cartMenuResponseDtoList = cartService.getCartMenuList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(cartMenuResponseDtoList);
+
+    }
 
 }
