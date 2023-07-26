cbd86c0f2bc8ba24d81c09881b8c18f64783d053
Sung Hyobin <hellmir610@gmail.com>
2023-07-26 19:36:04 +0900

    feat(Validation): Add a process validating the 'shop' table has a shop of same name with the shop to register

--- src/main/java/personal/delivery/shop/repository/ShopRepository.java
+++ src/main/java/personal/delivery/shop/repository/ShopRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import personal.delivery.shop.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    Shop findByName(String name);
 }
