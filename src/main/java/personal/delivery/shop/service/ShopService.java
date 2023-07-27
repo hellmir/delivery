4764e7203a6d9aa4961a19fd639b9a108e35998c
Sung Hyobin <hellmir610@gmail.com>
2023-07-27 21:19:12 +0900

    refactor(Shop Service): Change ShopService class to interface

--- src/main/java/personal/delivery/shop/service/ShopService.java
+++ src/main/java/personal/delivery/shop/service/ShopService.java
@@ -1,80 +1,14 @@
 package personal.delivery.shop.service;
 
-import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import personal.delivery.constant.Role;
-import personal.delivery.member.eneity.Member;
-import personal.delivery.member.repository.MemberRepository;
-import personal.delivery.shop.dto.ShopDto;
+import personal.delivery.shop.dto.ShopRequestDto;
 import personal.delivery.shop.dto.ShopResponseDto;
-import personal.delivery.shop.entity.Shop;
-import personal.delivery.shop.repository.ShopRepository;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class ShopService {
+public interface ShopService {
 
-    private final ShopRepository shopRepository;
-    private final MemberRepository memberRepository;
+    ShopResponseDto saveShop(ShopRequestDto shopDto);
 
-    public ShopResponseDto saveShop(ShopDto shopDto) {
-
-        Member member = memberRepository.findByEmail(shopDto.getEmail());
-
-        validateMemberIsExist(shopDto, member);
-
-        validateAuthority(member);
-
-        validateShopIsExist(shopDto);
-
-        Shop shop = Shop.builder()
-                .name(shopDto.getName())
-                .member(member)
-                .registrationTime(LocalDateTime.now())
-                .build();
-
-        Shop savedShop = shopRepository.save(shop);
-
-        ShopResponseDto shopResponseDto = new ShopResponseDto();
-
-        shopResponseDto.setId(savedShop.getId());
-        shopResponseDto.setName(savedShop.getName());
-        shopResponseDto.setEmail(savedShop.getMember().getEmail());
-        shopResponseDto.setRegistrationTime(savedShop.getRegistrationTime());
-
-        return shopResponseDto;
-
-    }
-
-    private void validateMemberIsExist(ShopDto shopDto, Member member) {
-
-        if (member == null) {
-            throw new EntityNotFoundException("해당 회원을 찾을 수 없습니다. (email: " + shopDto.getEmail() + ")");
-        }
-
-    }
-
-    private void validateAuthority(Member member) {
-
-        if (member.getRole().equals(Role.CUSTOMER)) {
-            throw new IllegalArgumentException("권한이 없습니다. (role: " + member.getRole() + ")");
-        }
-
-    }
-
-    private void validateShopIsExist(ShopDto shopDto) {
-
-        Shop shop = shopRepository.findByName(shopDto.getName());
-
-        if (shop != null) {
-            throw new IllegalStateException("해당 가게가 이미 존재합니다. (shopName: " + shopDto.getName() + ")");
-        }
-
-    }
+    List<ShopResponseDto> getAllShop();
 
 }
