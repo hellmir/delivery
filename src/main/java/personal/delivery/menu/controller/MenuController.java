2dde7e37406d984c7bbea3e7fdf5943ad3cf0dc4
Sung Hyobin <hellmir610@gmail.com>
2023-07-28 20:28:38 +0900

    rename(Menu Controller): Rename MenuController to ShopMenuController and split Controller of Menu domain
    Splited Controller to ShopMenuController and MenuController because the /menu URI is only used by getAllMenu method.
    As a result, duplicate (shopId}/menu URIs have been integrated into one in @RequestMapping.
    Also renamed the URI of menu domain from menu to menus.

---
+++ src/main/java/personal/delivery/menu/controller/MenuController.java
@@ -0,0 +1,30 @@
+package personal.delivery.menu.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import personal.delivery.menu.dto.MenuResponseDto;
+import personal.delivery.menu.service.MenuService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("menus")
+@RequiredArgsConstructor
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @GetMapping("menu")
+    public ResponseEntity<List<MenuResponseDto>> getAllMenu() {
+
+        List<MenuResponseDto> menuResponseDtoList = menuService.getAllMenu();
+
+        return ResponseEntity.status(HttpStatus.OK).body(menuResponseDtoList);
+
+    }
+
+}
