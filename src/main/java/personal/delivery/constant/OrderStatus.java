a1ae608010be480aaee2b5346334692235464924
Sung Hyobin <hellmir610@gmail.com>
2023-07-29 18:48:54 +0900

    feat(Descriptions of Constants): Add Korean descriptions of all constants

--- src/main/java/personal/delivery/constant/OrderStatus.java
+++ src/main/java/personal/delivery/constant/OrderStatus.java
@@ -1,5 +1,19 @@
 package personal.delivery.constant;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum OrderStatus {
-    WAITING, REFUSED, CANCELED, REFUNDED, COOKING, IN_DELIVERY, DELIVERED, REVIEWED
+
+    WAITING("접수 대기"),
+    REFUSED("주문 거절"),
+    CANCELED("주문 취소"),
+    REFUNDED("환불 완료"),
+    COOKING("조리 중"),
+    IN_DELIVERY("배달 중"),
+    DELIVERED("배달 완료"),
+    REVIEWED("리뷰 완료");
+
+    private final String description;
+
 }
