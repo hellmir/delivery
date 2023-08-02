3f14c73f17f49c1c5fce299776b9655b2c05d042
Sung Hyobin <hellmir610@gmail.com>
2023-08-02 21:57:27 +0900

    feat(Path Variable): Change the shopId in the OrderController class from inner field of OrderRequestDto to path variable of URI

--- src/main/java/personal/delivery/order/service/OrderService.java
+++ src/main/java/personal/delivery/order/service/OrderService.java
@@ -1,15 +1,18 @@
 package personal.delivery.order.service;
 
+import personal.delivery.menu.dto.MenuResponseDto;
 import personal.delivery.order.dto.OrderRequestDto;
 import personal.delivery.order.dto.OrderResponseDto;
 import personal.delivery.order.dto.OrderStatusChangeDto;
 
+import java.util.List;
+
 public interface OrderService {
 
-    OrderResponseDto takeOrder(OrderRequestDto orderRequestDto);
+    OrderResponseDto takeOrder(Long shopId, OrderRequestDto orderRequestDto);
 
     OrderResponseDto gerOrder(OrderRequestDto orderRequestDto);
 
-    OrderResponseDto changeOrderStatus(Long id, OrderStatusChangeDto orderStatusChangeDto);
+    OrderResponseDto changeOrderStatus(Long shopId, Long id, OrderStatusChangeDto orderStatusChangeDto);
 
 }
