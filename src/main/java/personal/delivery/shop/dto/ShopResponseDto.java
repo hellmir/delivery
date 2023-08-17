93553b1ddea02ce4a6cd7bccb3994cdd6a9d0530
Sung Hyobin <hellmir610@gmail.com>
2023-08-17 19:05:12 +0900

    feat(DTO): Replace the fields in DTOs from raw entities to specific fields and responseDTO

--- src/main/java/personal/delivery/shop/dto/ShopResponseDto.java
+++ src/main/java/personal/delivery/shop/dto/ShopResponseDto.java
@@ -1,15 +1,19 @@
 package personal.delivery.shop.dto;
 
 import lombok.Data;
+import personal.delivery.menu.dto.MenuResponseDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ShopResponseDto {
 
     private Long id;
     private String name;
-    private String email;
+    private Long memberId;
+    private String memberName;
+    private List<MenuResponseDto> menuResponseDtoList;
     private LocalDateTime registeredTime;
     private LocalDateTime updatedTime;
 
