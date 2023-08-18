6405fb6bf3e426143c8537793a8b62d54338f8ed
Sung Hyobin <hellmir610@gmail.com>
2023-08-18 23:53:45 +0900

    feat(Test): Add a test of findAllByRole method in MemberRepositoryTest

--- src/test/java/personal/delivery/member/repository/MemberRepositoryTest.java
+++ src/test/java/personal/delivery/member/repository/MemberRepositoryTest.java
@@ -71,4 +71,41 @@ class MemberRepositoryTest {
 
     }
 
+    @DisplayName("Role을 통해 원하는 분류의 회원 목록을 조회할 수 있다.")
+    @Test
+    void findAllByRole() {
+
+        // given
+
+        Member member1 = createMember("홍길동", "abcd@abc.com", "1234", SELLER);
+
+        Member member2 = createMember("고길동", "abcd@abcd.com", "1234", CUSTOMER);
+
+        Member member3 = createMember("김길동", "abcd@abcde.com", "1234", SELLER);
+
+        Member member4 = createMember("길동", "abcd@abcdef.com", "1234", CUSTOMER);
+
+        memberRepository.saveAll(List.of(member1, member2, member3, member4));
+
+        // when
+        List<Member> selectedMembers1 = memberRepository.findAllByRole(SELLER);
+        List<Member> selectedMembers2 = memberRepository.findAllByRole(CUSTOMER);
+
+        // then
+        assertThat(selectedMembers1).hasSize(2)
+                .extracting("name", "email", "role")
+                .containsExactlyInAnyOrder(
+                        tuple("홍길동", "abcd@abc.com", SELLER),
+                        tuple("김길동", "abcd@abcde.com", SELLER)
+                );
+
+        assertThat(selectedMembers2).hasSize(2)
+                .extracting("name", "email", "role")
+                .containsExactlyInAnyOrder(
+                        tuple("고길동", "abcd@abcd.com", CUSTOMER),
+                        tuple("길동", "abcd@abcdef.com", CUSTOMER)
+                );
+
+    }
+
 }
